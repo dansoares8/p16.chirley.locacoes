75f99911738039261316b8c4815bea091f206857
dansoares8 <103952709+dansoares8@users.noreply.github.com>
2025-10-03 19:18:48 -0300

    up

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chirley Locações</title>
+
+    <!-- meta seo --> 
+     <meta name="description" content="Chirley Locações, Locação de Imóveis Residenciais, Comerciais e Espaços para Eventos">
+     <meta name="keywords" content="Chirley Locações, Locação, Imóveis, Residenciais, Casas, Aluguel de Casas, Aluguel para Eventos, Clube, Chácara, Férias, Descanso">
+</head>
+<body>
+
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="#">Principal</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li><a href="#">Principal</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header><!-- menu desktop e mobile -->
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu melhor e-mail ?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="acao" value="Cadastrar!">
+        </form>
+    </section><!-- banner-principal -->
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50">
+                <h2>Guilherme C. Grillo</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, molestiae dolorem nam vitae fuga, eos, amet sed totam necessitatibus adipisci quasi. Excepturi perspiciatis debitis qui autem eaque. Beatae, maxime ullam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, molestiae dolorem nam vitae fuga, eos, amet sed totam necessitatibus adipisci quasi. Excepturi perspiciatis debitis qui autem eaque. Beatae, maxime ullam!</p>
+            </div><!--w50-->
+
+            <div class="w50">
+                <img src="images/autor.jpg" alt="">
+            </div><!--w50-->
+        </div>
+    </section><!-- descricao-autor -->
+
+
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+
+        </div>
+    </section><!-- especialidades -->
+
+
+
+</body>
+</html>
